dcb80b1efdd08795743f520810794cd0e3580cbe
ltankey <luke.tankey@gmail.com>
2016-05-10 16:12:06 +1000

    change api template reference to a string

--- app/jobs/node_create_job.rb
+++ app/jobs/node_create_job.rb
@@ -10,15 +10,18 @@ class NodeCreateJob < ApplicationJob
       node = Node.find_by(uuid: response["uuid"]) || Node.new
       node.name = response["title"]
       node.uuid = response["uuid"]
-      node.template = Template.first
+      node.template = response["field_template"]["und"][0]["value"]
       node.section = Section.first
+
       if not node.content_block
         node.content_block = ContentBlock.new()
       end
+
       node.content_block.body = response["body"]["und"][0]["value"]
       node.content_block.unique_id = response["uuid"] + "_body"
       node.content_block.save()
       node.save()
     end
+
   end
 end
