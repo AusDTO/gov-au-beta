47292bd22863b22dd7bd9fcf02a957e4b57d019b
Alexander Sadleir <maxious@users.noreply.github.com>
2016-05-10 11:43:34 +1000

    [skip ci] remove generic tests

--- spec/models/agency.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Agency, type: :model do
-
-  it { should have_many :nodes }
-  it { should validate_uniqueness_of(:name).case_insensitive }
-  it { should validate_uniqueness_of(:slug).case_insensitive }
-  
-end
