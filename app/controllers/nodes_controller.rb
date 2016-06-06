03479e34fb7b800f9a211874cf68a33dad728323
ltankey <luke.tankey@gmail.com>
2016-06-06 16:31:14 +1000

    add new, edit, update and create actions for nodes

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -11,4 +11,55 @@ class NodesController < ApplicationController
 
     render_node @node, @section
   end
+
+  def new
+    @parent = Node.find(params[:parent]) if params[:parent] else nil
+
+    if @parent
+      @node = @parent.children.build
+      @node.section = @parent.section
+    else
+      @section = Section.find(params[:section])
+      @node = @section.nodes.build
+    end
+
+    @form = NodeForm.new(@node)
+  end
+
+  def create
+    @form = NodeForm.new(Node.new)
+
+    if @form.validate(params.require(:node).permit!)
+
+      @form.save do |hash|
+        node = Node.new(hash)
+        # TODO: UUID may not be required
+        node.uuid = SecureRandom.uuid
+        node.save
+      end
+
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @node = Node.find(params[:id])
+    @form = NodeForm.new(@node)
+  end
+
+  def update
+    @node = Node.find(params[:id])
+    @form = NodeForm.new(@form)
+
+    if @form.validate(params.require(:node).permit!)
+      @form.save do |hash|
+        @node.update_attributes(hash)
+        @node.save!
+      end
+    else
+      render :edit
+    end
+
+  end
 end
