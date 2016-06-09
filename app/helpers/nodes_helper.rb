dbde657ff7b8b3f84472d10642128596a1420e2e
James Sadler <freshtonic@gmail.com>
2016-06-09 15:09:54 +1000

    SITES-134 fix Node permissions

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -1,19 +1,5 @@
 module NodesHelper
 
-  def find_node!(section, nodes_path)
-    node = nil
-
-    nodes_path.split('/').each_with_index do |slug, idx|
-      if node.nil? && idx.zero?
-        node = section.nodes.with_state(:published).find_by! slug: slug
-      else
-        node = node.children.with_state(:published).find_by! slug: slug
-      end
-    end
-
-    node
-  end
-
   def render_node(node, section)
     if section.layout.present?
       render "templates/#{node.template}", layout: section.layout
