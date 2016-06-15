f864b098e1709f92fdd7c7a148e113679106f0fa
ltankey <luke.tankey@gmail.com>
2016-06-15 16:39:33 +1000

    display list of nodes for a section in editorial context

--- app/controllers/editorial/sections_controller.rb
+++ app/controllers/editorial/sections_controller.rb
@@ -1,8 +1,19 @@
 module Editorial
   class SectionsController < EditorialController
-    def index
-      authorize! :view, :editorial_page
-      @sections = Section.all.order(:name)
+    def show
+      @section = Section.find_by!(slug: params[:section])
+
+
+      if params[:filter].present?
+        case params[:filter]
+          when 'submissions'
+            @filter = 'submissions'
+          when 'my_pages'
+            @filter = 'my_pages'
+          else
+            @filter = nil
+        end
+      end
     end
   end
 end
