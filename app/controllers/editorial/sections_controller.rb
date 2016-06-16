0b3258ff3ba62cb56485e2eee33b700fa3e40855
ltankey <luke.tankey@gmail.com>
2016-06-16 10:57:44 +1000

    fetch filter from params instead of using case statement

--- app/controllers/editorial/sections_controller.rb
+++ app/controllers/editorial/sections_controller.rb
@@ -2,18 +2,7 @@ module Editorial
   class SectionsController < EditorialController
     def show
       @section = Section.find_by!(slug: params[:section])
-
-
-      if params[:filter].present?
-        case params[:filter]
-          when 'submissions'
-            @filter = 'submissions'
-          when 'my_pages'
-            @filter = 'my_pages'
-          else
-            @filter = nil
-        end
-      end
+      params.fetch(:filter, nil).permit(:submissions, :my_pages)
     end
   end
 end
