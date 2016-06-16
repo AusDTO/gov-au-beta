5a12bfbc7a53a38b87374930caffb77c110831da
Toby Hede <tobyhede@info-architects.net>
2016-06-16 12:06:47 +1000

    Add rspec

--- engines/synergy/lib/synergy/engine.rb
+++ engines/synergy/lib/synergy/engine.rb
@@ -2,6 +2,10 @@ module Synergy
   class Engine < ::Rails::Engine
     isolate_namespace Synergy
 
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths['db/migrate'].expanded.each do |expanded_path|
