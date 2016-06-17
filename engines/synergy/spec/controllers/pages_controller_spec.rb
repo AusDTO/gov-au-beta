f1158bfc214e9405da557de3d002e8cac19bae2e
Toby Hede <tobyhede@info-architects.net>
2016-06-17 14:32:28 +1000

    Fix the test

--- engines/synergy/spec/controllers/pages_controller_spec.rb
+++ engines/synergy/spec/controllers/pages_controller_spec.rb
@@ -4,20 +4,10 @@ RSpec.describe Synergy::PagesController, :type => :controller do
 
   routes { Synergy::Engine.routes }
 
-  # config.before(:each) { @routes = UserManager::Engine.routes }
-  #
-  # before do
-  #   @routes = Synergy::Engine.routes
-  # end
-
-
   describe 'GET #show' do
-    before do
-      root = Synergy::Node.create!(slug: '/')
-      parent = root.children.create!(slug: 'blah')
-      parent.children.create!(slug: 'vtha')
-    end
-    # let! (:node) { Synergy::Node.create!(slug: 'blah') }
+    let(:root)    { Synergy::Node.create!(slug: '') }
+    let(:parent)  { root.children.create!(slug: 'blah') }
+    let!(:node)    { parent.children.create!(slug: 'vtha') }
 
     it "should return a page" do
       get :show, path: 'blah/vtha'
