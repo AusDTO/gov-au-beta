ed28ee9d594c4adbff6919c00d902b43ee8db1d6
Toby Hede <tobyhede@info-architects.net>
2016-06-17 15:35:06 +1000

    Add content extra to node display

--- engines/synergy/app/helpers/synergy/pages_helper.rb
+++ engines/synergy/app/helpers/synergy/pages_helper.rb
@@ -9,27 +9,11 @@ module Synergy
       if node.content
         if node.content['body']
           node.content['body']['value']
+        elsif node.content['extra']
+          node.content['extra']['value']
         end
       end
     end
-
-    def tree_view(label_method = :to_s,  node = nil, level = -1)
-     if node.nil?
-       puts "root"
-       nodes = roots
-     else
-       label = "|_ #{node.send(label_method)}"
-       if level == 0
-         puts " #{label}"
-       else
-         puts " |#{"    "*level}#{label}"
-       end
-       nodes = node.children
-     end
-     nodes.each do |child|
-       tree_view(label_method, child, level+1)
-     end
-   end
 
   end
 end
