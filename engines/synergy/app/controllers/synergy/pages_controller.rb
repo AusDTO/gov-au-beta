31eb3387f3250d2403195180a06b0df31b460c65
Toby Hede <tobyhede@info-architects.net>
2016-06-20 13:58:54 +1000

    Actual recursive menu structure.

--- engines/synergy/app/controllers/synergy/pages_controller.rb
+++ engines/synergy/app/controllers/synergy/pages_controller.rb
@@ -2,14 +2,15 @@ require_dependency "synergy/application_controller"
 
 module Synergy
   class PagesController < ApplicationController
-    attr_accessor :node
-    helper_method :node
-
+    
     layout 'synergy/layouts/application'
 
     def show
+      @root_node = Synergy::Node.find_by!(path: '/')
+
       path = params[:path].blank? ? '/' : "/#{params[:path]}"
       @node = Synergy::Node.find_by!(path: path)
+
       render action: :show
     end
 
