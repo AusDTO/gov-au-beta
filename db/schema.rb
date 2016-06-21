a84c4cc49ef16a775bc8d6533672c42906b94294
ltankey <luke.tankey@gmail.com>
2016-06-21 13:36:57 +1000

    update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620001701) do
+ActiveRecord::Schema.define(version: 20160620061159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
