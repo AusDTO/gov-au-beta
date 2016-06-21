7c39fc446b656d62de89a3b33063544baf77805d
ltankey <luke.tankey@gmail.com>
2016-06-21 12:58:52 +1000

    add owner_of transient to user fabricator

--- spec/fabricators/user.rb
+++ spec/fabricators/user.rb
@@ -1,7 +1,7 @@
 Fabricator(:user) do
   email { Fabricate.sequence(:email) { |i| "user-#{i}@example.com" } }
   password { "qwerty1234" }
-  transient :is_admin, :author_of, :reviewer_of
+  transient :is_admin, :author_of, :reviewer_of, :owner_of
 
   after_build do |user, transients|
     if transients[:is_admin]
@@ -12,6 +12,10 @@ Fabricator(:user) do
     end
     if transients[:reviewer_of]
       user.add_role(:reviewer, transients[:reviewer_of])
+    end
+
+    if transients[:owner_of]
+      user.add_role(:owner, transients[:owner_of])
     end
   end
 end
