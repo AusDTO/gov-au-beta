ab49ce03c919dcbedf63eadc2bfb7b4583ce0bc3
Brendan Gordon <brendan.gordon@digital.gov.au>
2016-06-21 12:27:45 +1000

    Add collaborators page

--- app/controllers/editorial/sections_controller.rb
+++ app/controllers/editorial/sections_controller.rb
@@ -4,6 +4,20 @@ module Editorial
       @section = Section.find_by!(slug: params[:section])
       @filter = %w(my_pages submissions).detect { |f| f == params[:filter] }
     end
+
+    def collaborators
+      @section = Section.find_by!(slug: params[:section])
+
+      @pending = Request.where(section: @section, state: :requested)
+
+      #Find all the collaborators and roles on @section
+      @users_and_roles = Hash.new([])
+      Section.find_roles.pluck(:name).uniq.each do |role|
+        User.with_role(role, @section).each do |user|
+          @users_and_roles[user] += [role]
+        end
+      end
+    end
   end
 end
 
