39d30c349293176c713c5642c93de9f19cce6ad2
James Sadler <freshtonic@gmail.com>
2016-07-05 11:24:05 +1000

    SITES-279 break importer into smaller methods

--- lib/synergy/cms_import.rb
+++ lib/synergy/cms_import.rb
@@ -29,37 +29,46 @@ module Synergy
     def run
       with_muted_logging do
         ActiveRecord::Base.transaction(isolation: :read_committed) do
-          @adapter.log "deleting existing nodes"
-          Node.where(section: @adapter.section).delete_all
-          @adapter.log "finished deleting existing nodes"
-
+          clear_existing_nodes
           @adapter.run do |node_data|
-            source_parts = node_data[:path].split("/").select{|p| !p.blank?}
-            parts        = source_parts
-
-            leaf = parts.reduce(@adapter.section) do |node,part|
-              node.children.find_or_create_by!(
-                section: @adapter.section,
-                parent_id: parent_id(node),
-                slug: part
-              ) do |child|
-                child.name        = part.gsub("-", " ").humanize
-                child.state       = :published
-                child.cms_api_url = node_data[:cms_api_url]
-              end
-            end
-
-            leaf.name         = node_data[:title]
-            leaf.template     = "general_content"
-            leaf.cms_url      = node_data[:cms_ref]
-            leaf.content_body = translate_absolute_links(absolutify_image_links(node_data[:content]))
-            leaf.save!
+            parts = node_data[:path].split("/").select{|p| !p.blank?}
+            leaf = build_path_to_leaf_node(parts, node_data) 
+            write_leaf_content(leaf, node_data)
           end
         end
       end
     end
 
     private
+
+    def write_leaf_content(leaf, node_data)
+      leaf.update_attributes!({
+        :name         => node_data[:title],
+        :template     => "general_content",
+        :cms_url      => node_data[:cms_ref],
+        :content_body => translate_absolute_links(absolutify_image_links(node_data[:content])),
+      })
+    end
+
+    def build_path_to_leaf_node(parts, node_data)
+      parts.reduce(@adapter.section) do |node,part|
+        node.children.find_or_create_by!(
+          section: @adapter.section,
+          parent_id: parent_id(node),
+          slug: part
+        ) do |child|
+          child.name        = part.gsub("-", " ").humanize
+          child.state       = :published
+          child.cms_api_url = node_data[:cms_api_url]
+        end
+      end
+    end
+
+    def clear_existing_nodes
+      @adapter.log "deleting existing nodes"
+      Node.where(section: @adapter.section).delete_all
+      @adapter.log "finished deleting existing nodes"
+    end
 
     def with_muted_logging
       logger_level = ActiveRecord::Base.logger.level
