508feda2a5b970815f6366bbb6844da0d244ff8c
Toby Hede <tobyhede@info-architects.net>
2016-07-06 12:27:51 +1000

    Add minister model and controllers

---
+++ app/models/minister.rb
@@ -0,0 +1,22 @@
+class Minister < Section
+  resourcify
+
+  has_and_belongs_to_many :agencies,
+        class_name: 'Agency',
+        join_table: :section_connections,
+        foreign_key: :section_id,
+        association_foreign_key: :connection_id
+
+  has_and_belongs_to_many :departments,
+        class_name: 'Department',
+        join_table: :section_connections,
+        foreign_key: :section_id,
+        association_foreign_key: :connection_id
+
+  has_and_belongs_to_many :topics,
+        class_name: 'Topic',
+        join_table: :section_connections,
+        foreign_key: :section_id,
+        association_foreign_key: :connection_id
+
+end
