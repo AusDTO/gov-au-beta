aebd85974872fcf6401886a4a2464b3ba45662d1
ltankey <luke.tankey@gmail.com>
2016-07-08 09:58:10 +1000

    direct circleci to do blue/green deploy

--- bin/deploy-staging.sh
+++ bin/deploy-staging.sh
@@ -8,10 +8,10 @@ set -x
 
 # Update the blue app
 cf unmap-route gov-au-beta-blue apps.staging.digital.gov.au -n gov-au-beta
-cf push gov-au-beta-blue
+cf push gov-au-beta-blue || cf push gov-au-beta-blue || cf push gov-au-beta-blue
 cf map-route gov-au-beta-blue apps.staging.digital.gov.au -n gov-au-beta
 
 # Update the green app
 cf unmap-route gov-au-beta-green apps.staging.digital.gov.au -n gov-au-beta
-cf push gov-au-beta-green
+cf push gov-au-beta-green || cf push gov-au-beta-green || cf push gov-au-beta-green
 cf map-route gov-au-beta-green apps.staging.digital.gov.au -n gov-au-beta
