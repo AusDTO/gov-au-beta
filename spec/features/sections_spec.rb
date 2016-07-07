d53f7309567d4b0cf80427fc211dc3f090997bfd
Toby Hede <tobyhede@info-architects.net>
2016-07-07 11:24:20 +1000

    Add spec to test menu generation for published nodes

--- spec/features/sections_spec.rb
+++ spec/features/sections_spec.rb
@@ -24,7 +24,24 @@ RSpec.describe "section features", :type => :feature do
       before do
         login_as(user)
       end
+    end
 
+    context 'when a node is in state' do
+      let!(:one) { Fabricate(:node, name: 'blah', state: 'published', section: root) }
+      let!(:two) { Fabricate(:node, name: 'vtha', state: 'draft', section: root) }
+
+      it 'navigation does not display draft node' do
+        visit section_path(root)
+        within('nav.primary-nav') do
+          expect(find_link(one.name)[:href]).to eq(nodes_path(section: one.section, path: one.path))
+          expect{ find_link(two.name)[:href] }.to raise_error Capybara::ElementNotFound
+        end
+        within('article') do
+          expect(find_link(one.name)[:href]).to eq(nodes_path(section: one.section, path: one.path))
+          expect{ find_link(two.name)[:href] }.to raise_error Capybara::ElementNotFound
+        end
+
+      end
 
     end
   end
