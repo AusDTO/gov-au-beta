e137e3f2596caba86aab72101694103c669fe2d5
tessereth <teresa.bradbury@digital.gov.au>
2016-07-26 11:29:15 +1000

    Use the correct method to determine if the review buttons should be displayed

--- app/helpers/submissions_helper.rb
+++
@@ -1,6 +0,0 @@
-module SubmissionsHelper
-  def can_review?
-    return false unless submission.submitted?
-    submission.section.present? && current_user.has_role?(:reviewer, submission.section)
-  end
-end
