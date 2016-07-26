6f8678370ccf36c825ad6f6ab95d79d44901c4b2
ltankey <luke.tankey@gmail.com>
2016-07-27 08:49:32 +1000

    show summary field on section_home form and template

--- app/forms/section_home_form.rb
+++ app/forms/section_home_form.rb
@@ -1,6 +1,7 @@
 class SectionHomeForm < Reform::Form
   property :name
 
+  property :short_summary
   #TODO figure out a way to combine these to DRY up future content block declarations
   property :content_body
 end
