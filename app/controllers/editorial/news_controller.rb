527129f4b6254f2f7597a744be854639fb0df19e
ltankey <luke.tankey@gmail.com>
2016-07-26 13:28:24 +1000

    reject blank section_ids when processing article distributions

--- app/controllers/editorial/news_controller.rb
+++ app/controllers/editorial/news_controller.rb
@@ -33,7 +33,7 @@ module Editorial
         if current_user.is_member?(Section.find(@form.section_id))
           # TODO: move this into a form validator
           # Prevents distribution list being created for publisher
-          @form.section_ids.reject! do |s_id|
+          @form.section_ids.reject!(&:blank?).reject! do |s_id|
             s_id == @form.section_id ||
                 !current_user.is_member?(Section.find(s))
           end
@@ -68,7 +68,7 @@ module Editorial
 
       if @form.validate(params.require(:node))
         if current_user.is_member?(Section.find(@form.section_id))
-          @form.section_ids.reject! do |s|
+          @form.section_ids.reject!(&:blank?).reject! do |s|
             s == @form.section_id.to_s ||
                 !current_user.is_member?(Section.find(s))
           end
