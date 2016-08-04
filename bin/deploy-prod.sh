e0f3ea982f3974f190e8724b0221d451ced58359
ltankey <luke.tankey@gmail.com>
2016-08-04 10:26:36 +1000

    set circle tag and sha1 as part of deploy

--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -9,6 +9,12 @@ set -o pipefail
 # echo out each line of the shell as it executes
 set -x
 
+# Update the circle envvars for all apps
+cf set-env gov-au-beta-blue CIRCLE_TAG "$CIRCLE_TAG"
+cf set-env gov-au-beta-blue CIRCLE_SHA1 "$CIRCLE_SHA1"
+cf set-env gov-au-beta-green CIRCLE_TAG "$CIRCLE_TAG"
+cf set-env gov-au-beta-green CIRCLE_SHA1 "$CIRCLE_SHA1"
+
 # Update the blue app
 cf unmap-route gov-au-beta-blue apps.platform.digital.gov.au -n gov-au-beta
 cf push gov-au-beta-blue
--- bin/deploy-staging.sh
+++ bin/deploy-staging.sh
@@ -6,6 +6,10 @@ set -e
 # Output the commands we run
 set -x
 
+# Update the circle envvars for all apps
+cf set-env gov-au-beta-blue CIRCLE_SHA1 "$CIRCLE_SHA1"
+cf set-env gov-au-beta-green CIRCLE_SHA1 "$CIRCLE_SHA1"
+
 # Update the blue app
 cf unmap-route gov-au-beta-blue apps.staging.digital.gov.au -n gov-au-beta
 cf push gov-au-beta-blue
