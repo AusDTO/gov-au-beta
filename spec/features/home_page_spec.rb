3537285072bd47b5cedc884c72e883c157818c4e
Simon Schwartz <simon.schwartz0@gmail.com>
2016-08-23 14:23:21 +1000

    replace summary with short summary on homepage category lists (#420)
    * replace summary with short summary on homepage category lists
    
    * fix formatting of homepage transition tiles

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe 'home page', type: :feature do
   Warden.test_mode!
 
   let!(:root_node) { Fabricate(:root_node) }
-  let!(:real_category) { Fabricate(:category, placeholder: false, name: 'real category', summary: 'real summary')}
-  let!(:placeholder_category) { Fabricate(:category, placeholder: true, name: 'placeholder category', summary: 'placeholder summary')}
+  let!(:real_category) { Fabricate(:category, placeholder: false, name: 'real category', short_summary: 'real summary')}
+  let!(:placeholder_category) { Fabricate(:category, placeholder: true, name: 'placeholder category', short_summary: 'placeholder summary')}
 
   before do
     visit root_path
