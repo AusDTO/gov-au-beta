dbb8749d109078fcd731c32717f83a5cdeac1e18
Alex Sadleir <alex.sadleir@digital.gov.au>
2016-08-29 09:29:42 +1000

    Add a default name for RootSection

--- app/models/root_node.rb
+++ app/models/root_node.rb
@@ -7,10 +7,12 @@ class RootNode < Node
   end
 
   def associate_root_section
-    #create a root section if not created
-    if not RootSection.first
-      RootSection.new.save()
+    if not self.section_id
+      #create a root section if not created
+      if not RootSection.first
+        RootSection.new.save()
+      end
+      self.section_id = RootSection.first.id
     end
-    self.section_id = RootSection.first.id
   end
 end
--- app/models/root_section.rb
+++ app/models/root_section.rb
@@ -1,7 +1,14 @@
 class RootSection < Section
 resourcify
 
+after_initialize :assign_defaults, if: 'new_record?'
+
 def generate_home_node
   # no home_node needed
+end
+
+private
+def assign_defaults
+  self.name = "Root level pages"
 end
 end
