f0be65062643ec89632d02c4520ba10bb2237cb8
Brendan Gordon <brendan.gordon@digital.gov.au>
2016-09-07 14:32:24 +1000

    Added includes to section

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -9,11 +9,19 @@ module NewsHelper
 
   def news_articles(section: nil)
     if section.present?
-      section.news_articles.published.by_release_date.by_published_at
+      section.news_articles
+          .includes(:news_distributions,
+                    section: [:home_node],
+          )
+          .published
+          .by_release_date
+          .by_published_at
     else
       NewsArticle
           .includes(:news_distributions,
-                    sections: [:home_node]
+                    section: [:home_node],
+          # GIVES ActionView::Template::Error (The association scope 'related_sections' is instance dependent (the scope block takes an argument). Preloading instance dependent scopes is not supported.):
+          #           related_sections: [:home_node]
           )
           .published
           .by_release_date
