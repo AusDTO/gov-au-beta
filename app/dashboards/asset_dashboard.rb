4ad4fa8aaea318223afb7ed3e33a505362b41afb
ltankey <luke.tankey@gmail.com>
2016-10-05 08:47:27 +1100

    fix: add missing admin controller and update user ref in dashboard

--- app/dashboards/asset_dashboard.rb
+++ app/dashboards/asset_dashboard.rb
@@ -9,7 +9,7 @@ class AssetDashboard < Administrate::BaseDashboard
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
       id: Field::Number,
-      uploader: Field::HasOne,
+      uploader: Field::HasOne.with_options(class_name: "User"),
       asset_file_file_name: Field::String,
       asset_file_content_type: Field::String,
       asset_file_file_size: Field::String,
