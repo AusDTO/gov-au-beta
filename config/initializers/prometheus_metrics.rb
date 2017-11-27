3d9ffbe85ca4bf4523bd5ce52ad4b44b20092af3
Michael Richardson <michael.richardson@digital.gov.au>
2017-11-28 08:49:00 +1100

    upgrading prometheus as its broken. update circle-ci deployments to cloud foundry platform

--- config/initializers/prometheus_metrics.rb
+++ config/initializers/prometheus_metrics.rb
@@ -1,4 +1,8 @@
-require 'prometheus/client/rack/collector'
-require 'prometheus/client/rack/exporter'
-Rails.application.config.middleware.use Prometheus::Client::Rack::Collector
-Rails.application.config.middleware.use Prometheus::Client::Rack::Exporter
+# require 'prometheus/client/rack/collector'
+# require 'prometheus/client/rack/exporter'
+# Rails.application.config.middleware.use Prometheus::Client::Rack::Collector
+# Rails.application.config.middleware.use Prometheus::Client::Rack::Exporterrequire 'prometheus/middleware/collector'
+require 'prometheus/middleware/collector'
+require 'prometheus/middleware/exporter'
+Rails.application.config.middleware.use Prometheus::Middleware::Collector
+Rails.application.config.middleware.use Prometheus::Middleware::Exporter
