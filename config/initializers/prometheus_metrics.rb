5dc401e66240357878aacd7fa40a62c7b39fb5ce
Michael Richardson <michael.richardson@digital.gov.au>
2017-11-28 18:04:21 +1100

    remove commented out old prometheus configuration

--- config/initializers/prometheus_metrics.rb
+++ config/initializers/prometheus_metrics.rb
@@ -1,7 +1,3 @@
-# require 'prometheus/client/rack/collector'
-# require 'prometheus/client/rack/exporter'
-# Rails.application.config.middleware.use Prometheus::Client::Rack::Collector
-# Rails.application.config.middleware.use Prometheus::Client::Rack::Exporterrequire 'prometheus/middleware/collector'
 require 'prometheus/middleware/collector'
 require 'prometheus/middleware/exporter'
 Rails.application.config.middleware.use Prometheus::Middleware::Collector
