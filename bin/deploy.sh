e59191ef93bdb313d738f346fc3a64550a66a264
Michael Richardson <michael.richardson@digital.gov.au>
2017-11-28 18:20:18 +1100

    remove no longer used ci deployment scripts

--- bin/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Exit immediately if any commands return non-zero
-set -e
-
-# Output the commands we run
-set -x
-
-cf push gov-au-beta || cf push gov-au-beta || cf push gov-au-beta
